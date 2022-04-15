4b8acb12f340308207dfb87b3456818f4afbce62
David Kleitsch <dkleitsch@gmail.com>
2022-04-15 14:39:29 -0400

    Adding updated RScript file with deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,4 @@
+# Deliverable 1
 # Load dplyr
 library(dplyr)
 
@@ -9,3 +10,14 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Use summary function to determine p-value and r-squared value
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=car_mpg_table))
+
+
+# Deliverable 2
+# Import and read in the suspension coil file
+coil_table <- read.csv("Suspension_coil.csv",check.names = F, stringsAsFactors = F)
+
+# Create a total_summary dataframe using summarize()
+total_summary <- coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+# Create a lot_summary dataframe group_by() and summarize()
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
